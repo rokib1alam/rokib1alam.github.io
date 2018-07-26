4011c19aa0c2184141c26218662419015411e280
rokib1alam <rokibul1alam@gmail.com>
2018-07-25 22:05:02 -0700

    pull

--- day2.php
+++ day2.php
@@ -1,96 +1,7 @@
-<!DOCTYPE html>
-<!--
-Template Name: Indripress
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
--->
-<html>
-<head>
-<title>Rokibul</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
-</head>
-<body id="top">
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row0">
-  <div  class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <div class="fl_left">
-      <ul>
-      
-        <li><i class="fa fa-envelope-o"></i> rokibul1alam@gmail.com</li>
-      </ul>
-    </div>
-  
-    <!-- ################################################################################################ -->
-  </div>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row1">
-  <header id="header" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <h1><a href="index.html">Rokibul Alam</a></h1>
-    <p>FAB LAB Operator</p>
-    <!-- ################################################################################################ -->
-  </header>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row4">
-  <nav id="mainav" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <ul class="clear">
-      <li class="active"><a href="index.html">Home</a></li>
-      <li><a class="drop" href="#">Pages</a>
-        <ul>
-          <li><a href="day1.php">Day 1</a></li>
-          <li><a href="day2.php">Day 2</a></li>
-          <li><a href="day3.php">Day 3</a></li>
-          <li><a href="day4.php">Day 4</a></li>
-          <li><a href="day5.php">Day 5</a></li>
-          <li><a href="day6.php">Day 6</a></li>
-          <li><a href="day7.php">Day 7</a></li>
-          <li><a href="day8.php">Day 8</a></li>
-          <li><a href="day9.php">Day 9</a></li>
-        </ul>
+<?php include("header.php"); ?>
 
-      <li><a href="about.php">About Myself</a></li>
-    
-         </ul>
-    <!-- ################################################################################################ -->
-  </nav>
-</div>
-<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');">
-  <div id="pageintro" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-   
-      <ul class="slides">
-        <li>
-          <article>
-            
-            <h3 ><font size="44">DIGI FAB PROGRAM</font></h3>
-            <p><font color="yellow">Conducted by AKGEC SKILL FOUNDATION</font></p>
-            
-          </article>
-        </li>
-       
-        
-      </ul>
-  
-    <!-- ################################################################################################ -->
-  </div>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+
+
 
 <div class="wrapper row3">
   <div class="hoc container clear"> 
@@ -136,7 +47,7 @@ Licence URI: http://www.os-templates.com/template-terms
           <h6 class="heading">Creat our Downloded product</h6>
           <p>After the session will over we print our downloded stl file . We need stl file because 3d printer was not working without stl file. Every 3d printer need their own software.Cura software for Ultimakers, grabCAD software for straight forwards, Makerbot software for makerbot printer etc. 
 
-            </p>
+          	</p>
           <!-- <footer><a class="btn" href="#">Read More &raquo;</a></footer> -->
         </figcaption>
       </figure>
@@ -189,7 +100,7 @@ Licence URI: http://www.os-templates.com/template-terms
           <h6 class="heading">how to clone desktop github to github account?</h6>
           <p>Sign in to GitHub and GitHub Desktop before you start to clone. On GitHub, navigate to the main page of the repository. Under your repository name, click Clone or download. Click Open in Desktop to clone the repository and open it in GitHub Desktop. Click Choose... and, using Windows Explorer, navigate to a local path where you want to clone the repository. Click Clone.
 
-            </p>
+          	</p>
           <!-- <footer><a class="btn" href="#">Read More &raquo;</a></footer> -->
         </figcaption>
       </figure>
@@ -199,7 +110,7 @@ Licence URI: http://www.os-templates.com/template-terms
           <h6 class="heading">how to pull html in github</h6>
           <p>On GitHub, navigate to the main page of the repository. Upload files buttonUnder your repository name, click Upload files. Drag and drop areaDrag and drop the file or folder you'd like to upload to your repository onto the file tree. Commit message for your changeAt the bottom of the page, type a short, meaningful commit message that describes the change you made to the file. You can attribute the commit to more than one author in the commit message. For more information, see "Creating a commit with multiple co-authors." Commit branch optionsBelow the commit message fields, decide whether to add your commit to the current branch or to a new branch. If your current branch is master, you should choose to create a new branch for your commit and then create a pull request. Commit changes buttonClick Commit changes. Finnaly Search my github name in brouser to find my web pages.
 
-            </p>
+          	</p>
           <!-- <footer><a class="btn" href="#">Read More &raquo;</a></footer> -->
         </figcaption>
       </figure>
@@ -215,11 +126,6 @@ Licence URI: http://www.os-templates.com/template-terms
       <h6 class="heading">Finally thanks to Mr. Amit and Mr. Puneeth Raj to giving us a great Idea about 3d Printer and github technology </h6>
       
     </div>
-    <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
-<!-- JAVASCRIPTS -->
-<script src="layout/scripts/jquery.min.js"></script>
-<script src="layout/scripts/jquery.backtotop.js"></script>
-<script src="layout/scripts/jquery.mobilemenu.js"></script>
-<script src="layout/scripts/jquery.flexslider-min.js"></script>
-</body>
-</html>
+
+
+<?php include("footer.php"); ?>
---
+++ day8.php
@@ -0,0 +1,47 @@
+<?php include("header.php"); ?>
+
+
+
+
+<div class="wrapper row3">
+  <div class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle center">
+      <h6 class="heading">Day 8 Kuka Robotics</h6>
+      
+    </div>
+    <div class="posts">
+      <figure class="group">
+        <div><img src="#" alt=""></div>
+        <figcaption>
+          <h6 class="heading">About KUKA</h6>
+          <p> This session is taken by Mr Vikash Kumar, He is a versatile scholar and a trainer. He is a master trainer for industrial Robot. AKGEC has verities of Kuka lab. They have standard Robotic training cell, Rowboat Simulation Lab and mobile robot training cell. Whole day we learn how to handle that, how to control it and how to simulate it using the software.    
+      </figure>
+      <!-- ################################################################################################ -->
+     
+      
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+
+<div class="wrapper row3">
+  <div class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle center">
+      <h6 class="heading">Here is our daily activates.
+      <br>
+      
+    </div>
+
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <div class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle center">
+      <h6 class="heading">Thanks to Miss Amrita Joshi , and Miss. Komal Juneja to giving us a great Idea about those tropic.
+      
+    </div>
+
+
+<?php include("footer.php"); ?>
